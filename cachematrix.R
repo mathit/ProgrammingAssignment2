b48c957ed71bb2c66f88f950f3ec50eb3f240418
mathit <Johanjansen86@gmail.com>
2015-04-18 20:07:16 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
-        list(set = set, get = get,
-             setinverse = setinverse,
-             getinverse = getinverse)
+  ## Caching the matrix and its inverse
+  m <- NULL  ## m will be used to define the inverse
+  set <- function(y) {  
+    x <<- y     ## defining the matrix by the input
+    m <<- NULL  ## resetting m
+  }
+  get <- function() x  ## calling the matrix from  cache-memory
+  setinverse <- function(inverse) m <<- inverse  ## defining the inverse
+  getinverse <- function() m ## calling the inverse from cache-memory
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
-        }
-        data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse() ## defining the inverse
+  if(!is.null(m)) {                 ## if the inverse has succesfully been aquired 
+    message("getting cached data")  ## send message and return the inverse
+    return(m)
+  }
+  data <- x$get()   ## calling the matrix
+  m <- solve(data, ...) ## calculating the inverse
+  x$setinverse(m)   ## defining the inverse
+  m     ## returning the inverse
 }
